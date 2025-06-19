94e36e6b9d49f3cc8978fadce28744a4b7e37e6d
mukilan143 <mukilanmukilan174@gmail.com>
2025-06-19 09:39:07 +0530

    project completed

---
+++ src/context/Weathercontext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useEffect, useState } from "react";
+import { createRef } from "react";
+import { data } from "react-router";
+
+export const weathercontext = createContext();
+
+const Weathercontext = ({ children }) => {
+  const inputref = createRef();
+  const [weather, setweather] = useState();
+  const [temperature, settemperature] = useState();
+  const [description, setdescription] = useState();
+  const [name, setname] = useState("");
+
+  const [input, setinput] = useState("");
+
+  const getweatherdata = async () => {
+    if (input === "") {
+      alert("Please enter a city name");
+      return;
+    }
+    const apires = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=3fc2ed80259799707da42df549c384c2`
+    );
+    const data = await apires.json();
+    setweather(data.weather[0].main);
+    settemperature(data.main.temp);
+    setdescription(data.weather[0].description);
+    setname(data.name);
+    console.log(data);
+    // const {main,weather}=data;
+  };
+
+  const onfocus = () => {
+    inputref.current.focus();
+  };
+  useEffect(() => {
+    onfocus();
+  }, []);
+
+  return (
+    <weathercontext.Provider
+      value={{
+        input,
+        setinput,
+        getweatherdata,
+        data,
+        weather,
+        temperature,
+        description,
+        name,
+        inputref,
+      }}
+    >
+      {children}
+    </weathercontext.Provider>
+  );
+};
+
+export default Weathercontext;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from "react";
+import { weathercontext } from "../context/Weathercontext";
+import{HiSun,HiMoon}from 'react-icons/hi2'
+
+const Home = () => {
+  const {
+    setinput,
+    input,
+    getweatherdata,
+    data,
+    weather,
+    temperature,
+    description,
+    name,
+    inputref,
+  } = useContext(weathercontext);
+  console.log(data);
+
+  const [darkmode,setdarkmode]=useState(false);
+
+  // const [weatherdata, setweatherdata] = useState();
+  const onSubmit = () => {
+    setinput("");
+    getweatherdata();
+  };
+
+
+
+  
+  
+  return (
+    <section style={{
+    backgroundColor: darkmode ? "black" : "#38bdf8", // dark blue : sky blue
+    color: darkmode ? "white" : "white"
+  }}  className=" bg-sky-400 h-dvh text-white flex flex-col items-center transform transition-colors duration-300 ease-in-out">
+        <span  onClick={()=>{setdarkmode(!darkmode)}} className="fixed right-5 top-5 text-3xl transform transition duration-300 " >{darkmode?<span><HiSun/></span>:<HiMoon/>}</span>
+      <div className="*:mt-4">
+        <h1 className="text-4xl font-bold  md:text-5xl   ">Weather App🌥️</h1>
+        <input
+        ref={inputref}
+          value={input}
+          onChange={(e) => {
+            setinput(e.target.value);
+          }}
+          type="text"
+          placeholder="Enter City name"
+          className="  bg-white/20 backdrop-blur-sm shadow-lg placeholder:text-white outline px-1 py-2 text-lg outline-none border-2 rounded-lg "
+        />
+        <br />
+        <button style={{
+    backgroundColor: darkmode ? "white" : "black", // dark blue : sky blue
+    color: darkmode ? "black" : "white"
+  }}
+          className="bg-black p-2 rounded-lg hover:cursor-pointer transform hover:bg-white hover:text-black transition-all duration-300 ease-in-out text-white text-lg font-semibold shadow-lg"
+          onClick={onSubmit}
+        >
+          <a href=""></a>Get Report
+        </button>
+        <div className="  border-2 rounded-lg p-5  shadow-lg  bg-white/20 backdrop-blur-sm *:mt-2 *:text-2xl">
+          <h1 className="">Location : {name} </h1>
+          <h1 className="">Weather : {weather} </h1>
+          <h2>Temperature : {temperature} </h2>
+          <h2>Description : {description} </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
